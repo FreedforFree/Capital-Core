524d5f31f277a9321848d42586f3bed9ca8e69bf
Friedemann Baumgaertel <friedemann1801@gmail.com>
2023-03-27 11:07:24 +0200

    first build

--- capitalcore/client/src/Components/Main.tsx
+++ capitalcore/client/src/Components/Main.tsx
@@ -1,26 +1,37 @@
 import React from "react";
+import { useState } from "react";
 
 export default function Main() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+        console.log(e.target.value);
+    };
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+        console.log(e.target.value);
+    };
     function handleSignup(e: React.FormEvent) {
         e.preventDefault();
-        console.log(e.value)
-    }
-    function handleButtonPress (e) {
-        console.log(e.value)
+        console.log(email, password)
     }
     return(
         <section>
             <div className="signup-container">
-                <h3 className="process-step-text">1. Create free Account</h3>
+                <h3 className="process-step-text1">1. Create free Account</h3>
                 <div className="signup-container-form">
                     <form className="signup-form" onSubmit={handleSignup}>
                         <input required className="email-input" type="email"
-                                placeholder="E-Mail"/>
+                                placeholder="E-Mail" onChange={handleEmailChange}/>
                         <input required className="password-input" type="password"
-                                placeholder="Password" />
-                        <button className="signup-form-button" onClick={handleButtonPress}>Sign Up</button>
+                                placeholder="Password" onChange={handlePasswordChange}/>
+                        <button className="signup-form-button">Sign Up</button>
                     </form> 
                 </div>
+            </div>
+            <div className="tolerance-container">
+                <h3 className="process-step-text2">2. Select Risk Tolerance</h3>
             </div>
         </section>
     )
